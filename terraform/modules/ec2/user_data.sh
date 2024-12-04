5bb7da44ce295327285e09c125d2d8b533285040
YaroslavKSE <ikhomych@kse.org.ua>
2024-12-04 17:30:58 +0000

    Created terraform code for creating infrastructure for rocketdex application deployment

---
+++ terraform/modules/ec2/user_data.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Update system packages
+sudo dnf update -y
+
+# Install Docker
+sudo dnf install -y docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
+
+# Install kubectl
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.31/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.31/rpm/repodata/repomd.xml.key
+EOF
+sudo yum install -y kubectl
+
+# Install required dependencies
+sudo dnf install -y conntrack
+
+# Install Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+# Start Minikube
+sudo -u ec2-user minikube start --driver=docker --cpus=2 --memory=2048
+
+# Create directory for K8s manifests
+mkdir -p /k8s-manifests
+
+# Install Nginx
+sudo dnf install -y nginx
+
+# Wait briefly for minikube to be fully ready
+sleep 15
+
+# Get minikube IP and create Nginx configuration
+MINIKUBE_IP=$(sudo -u ec2-user minikube ip)
+cat << EOF | sudo tee /etc/nginx/conf.d/kubernetes.conf
+server {
+    listen 80;
+    server_name k8s.academichub.net;
+    location / {
+        proxy_pass http://${MINIKUBE_IP}:30000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+    }
+}
+EOF
+
+# Start Nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
---
+++ terraform/variables.tf
@@ -0,0 +1,57 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.2.0/24", "10.0.4.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["eu-north-1a", "eu-north-1b"]
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+  default     = "ami-02db68a01488594c5" # Amazon Linux 2023 AMI
+}
+
+variable "certificate_arn" {
+  description = "The arn of pregenerated SSL/TLS certificat"
+  type        = string
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+}
+
+variable "RocketDexK8s_Frontend_type" {
+  description = "RocketDexK8s EC2 instance type"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "RocketDexK8s_Frontend_count" {
+  description = "Number of frontend instances to create"
+  type        = number
+  default     = 1
+}
